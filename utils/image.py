b88ce41736e5657a4e85e66e2e48e3ffdb72b825
aspirantll <aspirantll@foxmail.com>
2021-03-12 01:10:48 +0800

    remove skimage dependency

--- utils/image.py
+++ utils/image.py
@@ -15,7 +15,6 @@ import numpy as np
 import cv2
 
 from scipy.sparse import lil_matrix, issparse
-from skimage.measure import find_contours
 
 
 def random_crop(img_size):
@@ -105,46 +104,6 @@ def load_rgb_image(img_path):
 
 def load_instance_image(img_path):
     return cv2.imread(img_path, cv2.IMREAD_ANYDEPTH)
-
-
-def mask2poly(mask, threshold=0.5):
-    """Takes a mask and returns a MultiPolygon
-    Parameters
-    ----------
-    mask : array
-        Sparse or dense array to identify polygon contours within.
-    threshold : float, optional
-        Threshold value used to separate points in and out of resulting
-        polygons. 0.5 will partition a boolean mask, for an arbitrary value
-        binary mask choose the midpoint of the low and high values.
-    Output
-    ------
-    MultiPolygon
-        Returns a MultiPolygon of all masked regions.
-    """
-    m = _reformat_mask(mask)[0]
-
-    if issparse(m):
-        m = np.array(m.astype('byte').todense())
-
-    if (m != 0).sum() == 0:
-        # If the plane is empty, just skip it
-        return []
-
-    # Add an empty row and column around the mask to make sure edge masks
-    # are correctly determined
-    expanded_dims = (m.shape[0] + 2, m.shape[1] + 2)
-    expanded_mask = np.zeros(expanded_dims, dtype=float)
-    expanded_mask[1:m.shape[0] + 1, 1:m.shape[1] + 1] = m
-
-    verts = find_contours(expanded_mask.T, threshold)
-
-    # Subtract off 1 to shift coords back to their real space,
-    # but also add 0.5 to move the coordinates back to the corners,
-    # so net subtract 0.5 from every coordinate
-    # verts = [np.subtract(x, 0.5) for x in verts]
-
-    return verts
 
 
 def _reformat_mask(mask):
